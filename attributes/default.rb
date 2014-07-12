964e8ab2dba209b48fd7cffec3389e6562d7a385
Frank-Robert Kline <frank.kline@gmail.com>
2014-07-11 21:34:30 -0700

    Include JCE for Java 8 download URL and SHA-256 checksum

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
+default["java_ext"]["jce"]["8"]["url"] = "http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip"
+default["java_ext"]["jce"]["8"]["checksum"] = "f3020a3922efd6626c2fff45695d527f34a8020e938a49292561f18ad1320b59"
 default["java_ext"]["jce"]["7"]["url"] = "http://download.oracle.com/otn-pub/java/jce/7/UnlimitedJCEPolicyJDK7.zip"
 default["java_ext"]["jce"]["7"]["checksum"] = "7a8d790e7bd9c2f82a83baddfae765797a4a56ea603c9150c87b7cdb7800194d"
 default["java_ext"]["jce"]["6"]["url"] = "http://download.oracle.com/otn-pub/java/jce_policy/6/jce_policy-6.zip"
