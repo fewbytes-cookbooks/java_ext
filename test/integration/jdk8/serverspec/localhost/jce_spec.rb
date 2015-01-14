71448a6e7feaf0030c0f3f6fdcdc2aa1e7814c28
Jay Zhu <tingjie.zhu@gmail.com>
2015-01-14 14:55:01 -0800

    Fix Kitchen test failures

--- test/integration/jdk8/serverspec/localhost/jce_spec.rb
+++ test/integration/jdk8/serverspec/localhost/jce_spec.rb
@@ -5,20 +5,20 @@ def java_home
 end
 
 describe 'java_ext::jce' do
-  context file('/opt/java_ext/jce/6/local_policy.jar') do
+  context file('/opt/java_ext/jce/8/local_policy.jar') do
     it { should be_file }
   end
-  context file('/opt/java_ext/jce/6/US_export_policy.jar') do
+  context file('/opt/java_ext/jce/8/US_export_policy.jar') do
     it { should be_file }
   end
 
   context file("#{java_home}/jre/lib/security/local_policy.jar") do
     it { should be_symlink }
-    it { should be_linked_to '/opt/java_ext/jce/6/local_policy.jar' }
+    it { should be_linked_to '/opt/java_ext/jce/8/local_policy.jar' }
   end
   context file("#{java_home}/jre/lib/security/US_export_policy.jar") do
     it { should be_symlink }
-    it { should be_linked_to '/opt/java_ext/jce/6/US_export_policy.jar' }
+    it { should be_linked_to '/opt/java_ext/jce/8/US_export_policy.jar' }
   end
 end
 
